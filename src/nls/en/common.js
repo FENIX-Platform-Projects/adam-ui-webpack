f49f781c1737371e7b578cf6cbd79aa3e545bc40
tanzm <Tanzila.Mohammad@fao.org>
2016-11-28 13:39:22 +0000

    Browse By Additions: Download/Print options outside if chart

--- src/nls/en/common.js
+++ src/nls/en/common.js
@@ -13,6 +13,15 @@ define(function () {
         "selector_select_all" : "Select All",
         "selector_select_none" : "Select None",
         "loading_in_progress" : "Loading in progress",
-        "completed" : "Completed"
+        "completed" : "Completed",
+        "print": "Print",
+        "download": "Download",
+        "data": "Download data as Excel file",
+        "csv": "Download data as CSV file",
+        "msexcel": "Download data as MS Excel file",
+        "png": "Download PNG image",
+        "jpeg": "Download JPEG image",
+        "svg": "Download SVG vector image",
+        "pdf": "Download PDF Document"
     }
 });
