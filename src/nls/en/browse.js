eb3b9862b495ad2a98e1168179393eb5fa711eab
tanzm <Tanzila.Mohammad@fao.org>
2016-12-07 10:00:43 +0000

    Browse By Additions

--- src/nls/en/browse.js
+++ src/nls/en/browse.js
@@ -8,10 +8,10 @@ define(function () {
         "filter_btn": "Update",
         "source": "Source",
         "and": "and",
-        "selections" : "Current Selections",
-        "backToTop" : "Back to Top",
+        "selections": "Current Selections",
+        "backToTop": "Back to Top",
         "development_indicators": "Development Indicators",
-        "text" : "Change me in 118n/root/browse.js",
+        "text": "Change me in 118n/root/browse.js",
         "sector": "By Sector",
         "country": "By Recipient Country",
         "donor": "By Resource Partner",
@@ -22,7 +22,13 @@ define(function () {
         "total_sector_country_oda_fao_region": "ODA in the Sector for the Region",
         "total_country_sector_oda_fao_region": "ODA in the Sector for the Region",
         "total_country_sector_oda_recipientcode": "ODA in the Sector for the Country",
-        "total_country_subsector_oda_recipientcode":"ODA in the Sub Sector for the Country",
-        "total_country_subsector_oda_fao_region": "ODA in the Sub Sector for the Region"
+        "total_country_subsector_oda_recipientcode": "ODA in the Sub Sector for the Country",
+        "total_country_subsector_oda_fao_region": "ODA in the Sub Sector for the Region",
+        "total_donor_recipient_oda_fao_region": "Total ODA from Resource Partner to that Region",
+        "total_donor_recipient_oda_recipientcode": "Total ODA from Resource Partner to Country",
+        "total_donor_recipient_sector_oda_fao_region": "Total ODA of Sector from Resource Partner in that Region",
+        "total_donor_recipient_sector_oda_recipientcode": "Total ODA of Sector from Resource Partner in that Country",
+        "OECD_AVG_fao_region": "OECD Average of ODA in that Sector in that Region",
+        "OECD_AVG_recipientcode": "OECD Average of ODA in that Sector in that Country"
     }
 });
