9d61127e6d680ae2817bf37323794e5547c10691
tanzm <Tanzila.Mohammad@fao.org>
2016-12-16 09:01:12 +0000

    Priorities CSS Table Updates

--- src/config/priority/config-table.js
+++ src/config/priority/config-table.js
@@ -17,20 +17,19 @@ define(function () {
                         "groupedRow": false,
                         "formatter": "localstring",
                         "showRowHeaders": true,
-                        // "values":["projectshortdescription"],
-                        //  "rows":[ "purposecode",  "projecttitle"],
                         "rows": ["recipientcode", "purposecode", "undaf_stated_priority", "cpf_stated_priority"],
                         "aggregations": [],
                         inputFormat: "fenixtool",
 
+                        // 878 total width
                         config: {
                             pageSize: 150,
                             autoSelectFirstRow: false,
                             columns: [
                                 {id: "recipientcode_EN", fieldIndex: "0", width: 150},
                                 {id: "purposecode_EN", fieldIndex: "1", width: 200},
-                                {id: "undaf_stated_priority", fieldIndex: "2", width: 300},
-                                {id: "cpf_stated_priority",fieldIndex: "3",  width: 300}
+                                {id: "undaf_stated_priority", fieldIndex: "2", width: 220},
+                                {id: "cpf_stated_priority",fieldIndex: "3",  width: 220}
                             ]
                         }
                     },
