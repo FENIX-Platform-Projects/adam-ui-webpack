a6f8171ec96d3d1b90650e94a8d25a59288c936b
tanzm <Tanzila.Mohammad@fao.org>
2016-12-17 18:22:24 +0000

    Comparative Advantage Additions

---
+++ src/nls/comp-advantage.js
@@ -0,0 +1,13 @@
+define([
+        "./en/comp-advantage"
+    ],
+    function (i18nEn) {
+
+        'use strict';
+
+        return {
+
+            en: i18nEn
+
+        }
+    });
---
+++ src/nls/en/comp-advantage.js
@@ -0,0 +1,10 @@
+define(function () {
+    'use strict';
+
+    return {
+        "comparative-advantage": "FAO Comparative Advantage",
+        "comp_advantage_desc_1": "The FAO Comparative Advantage is an objective output based on a series of calculations performed on data reported by resource partners to the OECD-DAC.",
+        "comp_advantage_desc_2": "Hovering over the column headers provides a general description otherwise a more detailed explanation is available",
+        "comp_advantage_desc_3": "here"
+    }
+});
